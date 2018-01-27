c96166fbe9f36bc1f811f154858ef323a6ce11cf
Adam Nickle <anickle060193@gmail.com>
2018-01-27 07:29:50 -0600

    Initial update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
-import './App.css';
 
-const logo = require('./logo.svg');
+import Titlebar from 'components/Titlebar';
+import Challenge from 'components/Challenge';
 
-class App extends React.Component {
-  render() {
+export default class App extends React.Component
+{
+  render()
+  {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="container">
+        <Titlebar />
+        <Challenge />
       </div>
     );
   }
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root') as HTMLElement
+  document.getElementById( 'root' ) as HTMLElement
 );
+
 registerServiceWorker();
