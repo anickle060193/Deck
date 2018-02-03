ed16563179ce7a50100491e0641be4746047171d
Adam Nickle <anickle060193@gmail.com>
2018-02-03 11:20:01 -0600

    Improve redux logging.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, Middleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 import rootReducer from 'store/reducers';
 import rootSaga from 'store/sagas';
@@ -9,10 +9,18 @@ export const configureStore = ( initialState?: RootState ) =>
 {
   const sagaMiddleware = createSagaMiddleware();
 
-  const middlewares = [
-    sagaMiddleware,
-    logger
+  const middlewares: Middleware[] = [
+    sagaMiddleware
   ];
+
+  if( process.env.NODE_ENV === 'development' )
+  {
+    middlewares.push( createLogger( {
+      collapsed: true,
+      diff: true,
+      duration: true
+    } ) );
+  }
 
   const store = createStore( rootReducer, applyMiddleware( ...middlewares ) );
 
